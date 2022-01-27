d3776e584fd39ed294a5304f0e2910683076b445
Amanda Azevedo <58051437+afazevedo@users.noreply.github.com>
2022-01-27 15:29:26 -0300

    PATAMAR feito

--- codes/change_years.py
+++ codes/change_years.py
@@ -18,7 +18,7 @@ def changeYears(arquivo, caso):
         myfile.write(filedata)
 
 def main():
-    changeYears("D:\\mndzvd\\Documentos\\GitHub\\tarefas-fgv-simulacoes\\deck_newave_seco\\MODIF.DAT", "SECO")
+    changeYears("D:\\mndzvd\\Documentos\\GitHub\\tarefas-fgv-simulacoes\\deck_newave_seco\\PATAMAR.DAT", "SECO")
         
 if __name__ == "__main__":
     main()   
